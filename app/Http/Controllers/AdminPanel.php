2917a6e4ed97430e8b36af41aaa4b5b52cf3821a
Adalbertus250 <killergames156@gmail.com>
2022-02-06 00:51:20 +0100

    Update AdminPanel.php

--- app/Http/Controllers/AdminPanel.php
+++ app/Http/Controllers/AdminPanel.php
@@ -3,12 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\TriedaC;
+use App\Models\TriedaA;
+use App\Models\TriedaD;
+use App\Models\TriedaB;
 use Illuminate\Http\Request;
 use App\Models\ludia;
 
 class AdminPanel extends Controller{
-/*    public function index(){
-        $ludia = TriedaC::all();
-        return view("admin-panel", ["people" => $ludia]);
-    }*/
+    
+    public function index(){
+        $model = TriedaC::all();
+        return view("admin-panel", ["people" => $model]);
+    }
+    
+    public function triedaA(){
+        $model = TriedaA::all();
+        return view("trieda_a", ["people" => $model]);
+    }
+    
+    public function triedaD(){
+        $model = TriedaA::all();
+        return view("trieda_a", ["people" => $model]);
+    }
+    
+    public function triedaB(){
+        $model = TriedaA::all();
+        return view("trieda_a", ["people" => $model]);
+    }
+    
 }
